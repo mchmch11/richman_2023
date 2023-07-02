64d8ebfa502fd86e24a297e211a21326e2d5a70f
JiangYilun <nnyilun@126.com>
2023-07-02 23:36:54 +0800

    jyl:Adding test scrpit

---
+++ test/1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    char str[100] = {};
+    int points, tool_num;
+    scanf("%s %d %d", str, &points, &tool_num);
+    printf("666\n");
+    return 0;
+}
---
+++ test/test.py
@@ -0,0 +1,226 @@
+import subprocess
+import concurrent.futures
+import random
+
+class Test:
+    def __init__(self, filePath:str, testName:str, testNum: int, hasIllegal:bool = False, ErrorRate:float = 0.05) -> None:
+        self._tests = []
+        self._filePath = filePath
+        self._testName = testName
+        self._testNum = testNum # test tasks num
+        self._hasIllegal = hasIllegal # whether the program generated illegal data
+        self._ErrorRate = ErrorRate # if illegal data is generated, the probability of illegal data generation
+        self._weights = [1 - ErrorRate, ErrorRate]
+        self._results = None # task results
+        self._colors = ['\033[32m', '\033[31m', '\033[34m', '\033[0m'] # ANSI color: front_green, front_red, front_blue, front_reset
+        self.__default_tests()
+
+    def set_tests(self, tests): # customize test
+        self._tests = tests
+
+    def __default_tests(self):
+        self._tests = [
+            {"input":[], "expected_output":[]}
+        ]
+
+    def __print_test_name(self, _end=''): # print test name
+        print(f'[{self._colors[2]}{self._testName}{self._colors[3]}]', end=_end)
+
+    def __gen_input(self): # generate special test input
+        pass
+
+    def __gen_expected_output(self): # generate special test expected output
+        pass
+
+    def gen_test(self): # generate special test
+        pass
+
+    def __run_test(self, input_data): # run single test
+        p = subprocess.Popen([self._filePath], stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+
+        for item in input_data:
+            p.stdin.write(item.encode())
+            p.stdin.flush()
+
+        stdout, _ = p.communicate()
+
+        return stdout.decode()
+
+    def create_test_tasks(self): # create test tasks, tesk num = self._testNum
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            self._results = executor.map(self.__run_test, [test["input"] for test in self._tests])
+
+        for test, output in zip(self._tests, self._results):
+            self.__print_test_name()
+            if output == test["expected_output"]:
+                print(f'{self._colors[0]}Test passed!{self._colors[3]}')
+            else:
+                print(f'{self._colors[1]}Test failed! input:{test["input"]}, expected_output:{repr(test["expected_output"])}, actual_output:{repr(output)}{self._colors[3]}')
+        
+
+class TestPointStore(Test): # test for point store
+    '''
+    point store test
+    input:one purchase operation, format: operation present_points present_item_num
+    output: ["点数不足，退出道具房\n", "退出道具房\n", "道具已满\n", "已购买xxx\n", f"已购买xxx\n点数不足，退出道具房\n", "非法输入\n", "点数不足，购买失败\n"]
+    example: input:['1', '50', '0'], expected_output:['已购买路障\n点数不足，退出道具房\n']
+    '''
+
+    def __init__(self, filePath:str, testNum: int, hasIllegal:bool = False, ErrorRate:float = 0.05) -> None:
+        super().__init__(filePath, "TestPointStore", testNum, hasIllegal, ErrorRate)
+        self.__default_tests()
+        if not hasIllegal:
+            self._weights = [1, 0]
+            self._ErrorRate = 0
+
+    def __default_tests(self):
+        self._tests = [
+            {"input":["1", "50", "0"], "expected_output":["已购买路障\n点数不足，退出道具房\n"]},
+        ]
+    
+    def gen_test(self): # generate special test
+        self.__gen_input()
+        self.__gen_expected_output()
+
+    def __gen_input(self):
+        self.tools = [['1', '2', '3'], ['-1', '0', '4', '1.23', '3.44']]
+        self.quit = [['F', 'f'], ['a', 'b', '#', 'q', '@']]
+        self.points = [(0, 200), (-5, -1)]
+        self.tools_num = [(0, 10), (-5, -1), (11, 15)]
+
+        self._tests = []
+        for i in range(self._testNum):
+            self._tests.append({})
+            input = ""
+
+            # there is a 60% probability of generating numbers and a 40% probability of generating 'f'
+            if random.uniform(0, 1) > 0.4: 
+                tools_range = random.choices(self.tools, self._weights)[0]
+                input = random.choice(tools_range)
+            else:
+                quit_range = random.choices(self.quit, self._weights)[0]
+                input = random.choice(quit_range)
+
+            # generate points
+            points_range = random.choices(self.points, self._weights)[0]
+            points = random.randint(*points_range)
+
+            # generate the number of tools
+            tools_num_range = random.choices(self.tools_num, [1 - self._ErrorRate, 0.5 * self._ErrorRate, 0.5 * self._ErrorRate])[0]
+            tools_number = random.randint(*tools_num_range)
+
+            self._tests[i]["input"] = [input, str(points), str(tools_number)]
+    
+    def __gen_expected_output(self):
+        self._point = [50, 30, 50]
+        self._item = ["路障", "机器娃娃", "炸弹"]
+
+        for test in self._tests:
+            if int(test["input"][1]) < 30: # insufficient points, exiting the store
+                test["expected_output"] = "点数不足，退出道具房\n"
+            elif test["input"][0].lower() == 'f': # input 'f'
+                test["expected_output"] = "退出道具房\n"
+            elif len(test["input"][0]) == 1 and test["input"][0].isdigit(): # input 1 or 2 or 3
+                if int(test["input"][2]) >= 10: # full item
+                    test["expected_output"] = "道具已满\n"
+                elif 0 <= int(test["input"][2]) < 10: # can buy item
+                    if int(test["input"][1]) - self._point[int(test["input"][0]) - 1] >= 30: 
+                        # sufficient points and remaining after purchase
+                        test["expected_output"] = f"已购买{self._item[int(test['input'][0]) - 1]}\n"
+                    elif 30 > int(test["input"][1]) - self._point[int(test["input"][0]) - 1] >= 0: 
+                        # Enough points, but not enough to buy again after purchase, automatic exit
+                        test["expected_output"] = f"已购买{self._item[int(test['input'][0]) - 1]}\n点数不足，退出道具房\n"
+                    else:
+                        test["expected_output"] = "点数不足，购买失败\n"
+            else:
+                test["expected_output"] = "非法输入\n"
+            
+
+class TestDice(Test):
+    def __init__(self, filePath:str, testNum: int, hasIllegal:bool = False, ErrorRate:float = 0.05) -> None:
+        super().__init__(filePath, "TestDice", testNum, hasIllegal, ErrorRate)
+        self.__default_tests()
+        if not hasIllegal:
+            self._weights = [1, 0]
+            self._ErrorRate = 0
+
+    def __default_tests(self):
+        self._tests = [
+            {"input":[], "expected_output":[""]},
+        ]
+
+
+class TestMap(Test):
+    def __init__(self, filePath:str, testNum: int, hasIllegal:bool = False, ErrorRate:float = 0.05) -> None:
+        super().__init__(filePath, "TestMap", testNum, hasIllegal, ErrorRate)
+        self.__default_tests()
+        if not hasIllegal:
+            self._weights = [1, 0]
+            self._ErrorRate = 0
+
+    def __default_tests(self):
+        self._tests = [
+            {"input":[], "expected_output":[]},
+        ]
+
+
+class TestBuyLand(Test):
+    def __init__(self, filePath:str, testNum: int, hasIllegal:bool = False, ErrorRate:float = 0.05) -> None:
+        super().__init__(filePath, "TestBuyLand", testNum, hasIllegal, ErrorRate)
+        self.__default_tests()
+        if not hasIllegal:
+            self._weights = [1, 0]
+            self._ErrorRate = 0
+
+    def __default_tests(self):
+        self._tests = [
+            {"input":[], "expected_output":[]},
+        ]
+
+
+class TestSellingLand(Test):
+    def __init__(self, filePath:str, testNum: int, hasIllegal:bool = False, ErrorRate:float = 0.05) -> None:
+        super().__init__(filePath, "TestSellingLand", testNum, hasIllegal, ErrorRate)
+        self.__default_tests()
+        if not hasIllegal:
+            self._weights = [1, 0]
+            self._ErrorRate = 0
+
+    def __default_tests(self):
+        self._tests = [
+            {"input":[], "expected_output":[]},
+        ]
+        
+
+class TestSellingLand(Test):
+    def __init__(self, filePath:str, testNum: int, hasIllegal:bool = False, ErrorRate:float = 0.05) -> None:
+        super().__init__(filePath, "TestSellingLand", testNum, hasIllegal, ErrorRate)
+        self.__default_tests()
+        if not hasIllegal:
+            self._weights = [1, 0]
+            self._ErrorRate = 0
+
+    def __default_tests(self):
+        self._tests = [
+            {"input":[], "expected_output":[]},
+        ]
+
+
+
+if __name__ == '__main__':
+    # point store test
+    # input:one purchase operation, format: operation present_points present_item_num
+    # output: ["点数不足，退出道具房\n", "退出道具房\n", "道具已满\n", "已购买xxx\n", f"已购买xxx\n点数不足，退出道具房\n", "非法输入\n", "点数不足，购买失败\n"]
+    # example: input:['1', '50', '0'], expected_output:['已购买路障\n点数不足，退出道具房\n']
+    
+
+    # create default tasks
+    # the tasks' details, plz see __default_tests function overloading
+    test_store = TestPointStore("./1.exe", 1)
+    test_store.create_test_tasks()
+
+    # create 10 random tasks
+    # randomly generated tasks may have poor quality
+    test_store = TestPointStore("./1.exe", 10)
+    test_store.gen_test()
+    test_store.create_test_tasks()
