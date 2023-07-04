00fabba255063b914745ed563cf364099b9f8f35
root <root@localhost.localdomain>
2023-07-04 11:41:15 +0800

    添加一个map

--- src/update.c
+++ src/update.c
@@ -4,6 +4,32 @@
 #include<stdio.h>
 #include<time.h>
 /*step函数*/
+int step(Player*player,int step)
+{
+    //TODO
+    return 0;
+}
+
+/*put block in [-10,10],and not in player*/
+int putBlock(Player*player,int num)
+{
+    //TODO
+    return 0;
+}
+
+/*clear tool in front 10*/
+int robotClear(Player*player)
+{
+    //TODO
+    return 0;
+}
+
+/*put bomb in [-10,10],and not in player and hospital*/
+int putBomb(Player*player,int num)
+{
+    return 0;
+}
+
 /*包含更新状态的函数，用于每回合初处理各种效果和游戏中购买土地*/
 /*生成随机数的函数*/
 int get_roll_number()
